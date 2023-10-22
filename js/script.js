d774d7691865cd6cec85e23adbdd864b47700f08
aashish_k <aashishkathat00@gmail.com>
2023-10-23 00:37:58 +0530

    implement smooth scroll and mobile nav

--- js/script.js
+++ js/script.js
@@ -10,6 +10,31 @@ btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open');
 });
 
+// Smooth scrolling
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+    // scroll to top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    // scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // close mobile nav
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
